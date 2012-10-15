23c3c52b72232b3a5680c38c89951cf89cc9b5f9
Hussein Patni <hussein.patni@gettyimages.com>
2012-10-15 11:41:54 -0600

    Initial project creation. Add Rspec dependency

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '32241c97b3e16b5884e4f61c591b9de6732e5b88a98fe84ed200b40cc420a561f17b334843a1ce7809e1270890a32a6930dc9012af14ff368822c2bb9e1d09bc'
