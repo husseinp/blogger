2e5f3548ba515a719e2f3648076e8324d257fdf3
Hussein Patni <hussein.patni@gettyimages.com>
2012-10-16 11:40:51 -0600

    Create some article CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,31 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    @article = Article.find params[:id]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find params[:id]
+  end
+
+  def update
+    @article = Article.find params[:id]
+    @article.update_attributes params[:article]
+  end
+
+  def create
+    @article = Article.new params[:article]
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    Article.find(params[:id]).destroy
+    redirect_to articles_path
   end
 
 end
