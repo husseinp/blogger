43635f03d9336d9f94bb7ad7aa040f4e70a2ee80
Hussein Patni <hussein.patni@gettyimages.com>
2012-10-16 14:42:18 -0600

    Add Flash messages on CRUD actions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,17 +16,17 @@ class ArticlesController < ApplicationController
 
   def update
     article.update_attributes params[:article]
-    redirect_to article_path(article)
+    redirect_to article_path(article), notice:"Article '#{article.title}' Updated!"
   end
 
   def create
     article.save
-    redirect_to article_path(article)
+    redirect_to article_path(article), notice:"You have successfully created a new Article entitled: '#{article.title}'."
   end
 
   def destroy
     article.destroy
-    redirect_to articles_path
+    redirect_to articles_path, notice:"You have successfully deleted the article entititled '#{article.title}'."
   end
 
 end
